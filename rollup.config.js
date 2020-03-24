d3a551da6afb290c965821b5f1ca76c94544be7e
ajboni <mail@aboni.dev>
2020-03-23 23:08:13 -0300

    autoreload for md files

--- rollup.config.js
+++ rollup.config.js
@@ -9,6 +9,7 @@ import pkg from "./package.json";
 import yaml from "@rollup/plugin-yaml";
 import postcss from "rollup-plugin-postcss";
 import autoprefixer from "autoprefixer";
+import glob from 'glob'
 import fs from "fs";
 
 const mode = process.env.NODE_ENV;
@@ -16,120 +17,135 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === "CIRCULAR_DEPENDENCY" &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
-  onwarn(warning);
+	(warning.code === "CIRCULAR_DEPENDENCY" &&
+		/[/\\]@sapper[/\\]/.test(warning.message)) ||
+	onwarn(warning);
+
 
 export default {
-  client: {
-    input: config.client.input(),
-    output: config.client.output(),
-    plugins: [
-      {
-        buildStart() {
-          const files = fs.readdirSync("./docs/");
-          files.forEach(file => {
-            this.addWatchFile("./docs" + file);
-          });
-        }
-      },
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			{
+				buildStart() {
+					var self = this
+					const docFolder = "./docs/"
+					glob(docFolder + '/**/*.md', null, function (err, files) {
+						files.forEach(file => {
+							self.addWatchFile(file);
+						})
+					})
+				}
+			},
+			replace({
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			yaml(),
+			svelte({
+				dev,
+				hydratable: true,
+				emitCss: true
+			}),
+			postcss({
+				// We explicity leave css out in order to keep using svelte local styles. Might change in future.
+				extensions: [".scss"],
+				plugins: [autoprefixer()],
+				// inject: true,
+				extract: "static/global.css"
+			}),
+			resolve({
+				browser: true,
+				dedupe: ["svelte"]
+			}),
+			commonjs(),
 
-      replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode)
-      }),
-      yaml(),
-      svelte({
-        dev,
-        hydratable: true,
-        emitCss: true
-      }),
-      postcss({
-        // We explicity leave css out in order to keep using svelte local styles. Might change in future.
-        extensions: [".scss"],
-        plugins: [autoprefixer()],
-        // inject: true,
-        extract: "static/global.css"
-      }),
-      resolve({
-        browser: true,
-        dedupe: ["svelte"]
-      }),
-      commonjs(),
+			legacy &&
+			babel({
+				extensions: [".js", ".mjs", ".html", ".svelte"],
+				runtimeHelpers: true,
+				exclude: ["node_modules/@babel/**"],
+				presets: [
+					[
+						"@babel/preset-env",
+						{
+							targets: "> 0.25%, not dead"
+						}
+					]
+				],
+				plugins: [
+					"@babel/plugin-syntax-dynamic-import",
+					[
+						"@babel/plugin-transform-runtime",
+						{
+							useESModules: true
+						}
+					]
+				]
+			}),
 
-      legacy &&
-        babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
-          runtimeHelpers: true,
-          exclude: ["node_modules/@babel/**"],
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: "> 0.25%, not dead"
-              }
-            ]
-          ],
-          plugins: [
-            "@babel/plugin-syntax-dynamic-import",
-            [
-              "@babel/plugin-transform-runtime",
-              {
-                useESModules: true
-              }
-            ]
-          ]
-        }),
+			!dev &&
+			terser({
+				module: true
+			})
+		],
 
-      !dev &&
-        terser({
-          module: true
-        })
-    ],
+		onwarn
+	},
 
-    onwarn
-  },
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			{
+				buildStart() {
+					var self = this
+					const docFolder = "./docs/"
+					glob(docFolder + '/**/*.md', null, function (err, files) {
+						files.forEach(file => {
+							self.addWatchFile(file);
+						})
+					})
+				}
+			},
 
-  server: {
-    input: config.server.input(),
-    output: config.server.output(),
-    plugins: [
-      yaml(),
+			yaml(),
 
-      replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode)
-      }),
-      svelte({
-        generate: "ssr",
-        dev
-      }),
-      resolve({
-        dedupe: ["svelte"]
-      }),
-      commonjs()
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules ||
-        Object.keys(process.binding("natives"))
-    ),
+			replace({
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode)
+			}),
+			svelte({
+				generate: "ssr",
+				dev
+			}),
+			resolve({
+				dedupe: ["svelte"]
+			}),
+			commonjs()
+		],
+		external: Object.keys(pkg.dependencies).concat(
+			require("module").builtinModules ||
+			Object.keys(process.binding("natives"))
+		),
 
-    onwarn
-  }
+		onwarn
+	}
 
-  // serviceworker: {
-  // 	input: config.serviceworker.input(),
-  // 	output: config.serviceworker.output(),
-  // 	plugins: [
-  // 		resolve(),
-  // 		replace({
-  // 			'process.browser': true,
-  // 			'process.env.NODE_ENV': JSON.stringify(mode)
-  // 		}),
-  // 		commonjs(),
-  // 		!dev && terser()
-  // 	],
+	// serviceworker: {
+	// 	input: config.serviceworker.input(),
+	// 	output: config.serviceworker.output(),
+	// 	plugins: [
+	// 		resolve(),
+	// 		replace({
+	// 			'process.browser': true,
+	// 			'process.env.NODE_ENV': JSON.stringify(mode)
+	// 		}),
+	// 		commonjs(),
+	// 		!dev && terser()
+	// 	],
 
-  // 	onwarn,
-  // }
+	// 	onwarn,
+	// }
 };
